84fd37af952be3dc5ebe5a76b71d8b128d5082bb
priprii <41253973+priprii@users.noreply.github.com>
2020-10-17 09:48:58 +0100

    Fixed flickering issues on character switch

--- Util/Party.cs
+++ Util/Party.cs
@@ -12,6 +12,7 @@ namespace GenshinOverlay {
         };
         public static int PartySize { get; set; } = 4;
         public static int SelectedCharacter { get; set; } = -1;
+        private static int _p2PauseStep = 0;
 
         public static int GetSelectedCharacter(IntPtr handle) {
             if(handle == IntPtr.Zero) { return -1; }
@@ -21,11 +22,14 @@ namespace GenshinOverlay {
             c1 = IMG.GetColorAt(handle, Config.PartyNumLocations["4 #2"].X, Config.PartyNumLocations["4 #2"].Y) == Color.FromArgb(255, 255, 255, 255) ? 1 : 0;
             c2 = IMG.GetColorAt(handle, Config.PartyNumLocations["4 #3"].X, Config.PartyNumLocations["4 #3"].Y) == Color.FromArgb(255, 255, 255, 255) ? 1 : 0;
             c3 = IMG.GetColorAt(handle, Config.PartyNumLocations["4 #4"].X, Config.PartyNumLocations["4 #4"].Y) == Color.FromArgb(255, 255, 255, 255) ? 1 : 0;
+            
             if(c0 + c1 + c2 + c3 == 4) {
                 return -1;
             } else if(c0 + c1 + c2 + c3 == 3) {
                 PartySize = 4;
                 return c0 == 0 ? 0 : c1 == 0 ? 1 : c2 == 0 ? 2 : 3;
+            } else if(c0 + c1 + c2 + c3 == 2 && PartySize == 4) {
+                return SelectedCharacter;
             } else {
                 c0 = IMG.GetColorAt(handle, Config.PartyNumLocations["3 #1"].X, Config.PartyNumLocations["3 #1"].Y) == Color.FromArgb(255, 255, 255, 255) ? 1 : 0;
                 c1 = IMG.GetColorAt(handle, Config.PartyNumLocations["3 #2"].X, Config.PartyNumLocations["3 #2"].Y) == Color.FromArgb(255, 255, 255, 255) ? 1 : 0;
@@ -33,12 +37,18 @@ namespace GenshinOverlay {
                 if(c0 + c1 + c2 == 2) {
                     PartySize = 3;
                     return c0 == 0 ? 0 : c1 == 0 ? 1 : 2;
+                } else if(c0 + c1 + c2 == 1 && PartySize == 3) {
+                    return SelectedCharacter;
                 } else {
                     c0 = IMG.GetColorAt(handle, Config.PartyNumLocations["2 #1"].X, Config.PartyNumLocations["2 #1"].Y) == Color.FromArgb(255, 255, 255, 255) ? 1 : 0;
                     c1 = IMG.GetColorAt(handle, Config.PartyNumLocations["2 #2"].X, Config.PartyNumLocations["2 #2"].Y) == Color.FromArgb(255, 255, 255, 255) ? 1 : 0;
                     if(c0 + c1 == 1) {
                         PartySize = 2;
+                        _p2PauseStep = 0;
                         return c0 == 0 ? 0 : 1;
+                    } else if(c0 + c1 == 0 && _p2PauseStep < 2) {
+                        _p2PauseStep++;
+                        return SelectedCharacter;
                     }
                 }
             }
